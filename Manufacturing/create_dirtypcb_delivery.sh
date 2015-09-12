1c8365d9faff273dc4cebe651df270a5b05ed6fa
Patrick Fallberg <patrick@fallberg.net>
2015-09-12 22:10:38 +0200

    Created manufacturing outputs
    Also created DirtyPCBs-formatted archives.
    Fixes #9

--- Manufacturing/create_dirtypcb_delivery.sh
+++ Manufacturing/create_dirtypcb_delivery.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-rm -f DIRTYPCB.ZIP
-cp *-F_Cu.gbr TOP_COPPER.GTL
-cp *-B_Cu.gbr BTM_COPPER.GBL
-cp *-F_Mask.gbr TOP_MASK.GTS
-cp *-B_Mask.gbr BTM_MASK.GBS
-cp *-F_SilkS.gbr TOP_SILK.GTO
-cp *-B_SilkS.gbr BTM_SILK.GBO
-cp *-Edge_Cuts.gbr BRD_EDGE.GBR
-cp *.drl BRD_DRILL.TXT
-zip -m DIRTYPCB.ZIP TOP_COPPER.GTL BTM_COPPER.GBL TOP_MASK.GTS BTM_MASK.GBS TOP_SILK.GTO BTM_SILK.GBO BRD_EDGE.GBR BRD_DRILL.TXT
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+set -e
+
+rm -f $1_DIRTYPCB.ZIP
+cp $1-F_Cu.gbr TOP_COPPER.GTL
+cp $1-B_Cu.gbr BTM_COPPER.GBL
+cp $1-F_Mask.gbr TOP_MASK.GTS
+cp $1-B_Mask.gbr BTM_MASK.GBS
+cp $1-F_SilkS.gbr TOP_SILK.GTO
+cp $1-B_SilkS.gbr BTM_SILK.GBO
+cp $1-Edge_Cuts.gbr BRD_EDGE.GBR
+cp $1.drl BRD_DRILL.TXT
+zip -m $1_DIRTYPCB.ZIP TOP_COPPER.GTL BTM_COPPER.GBL TOP_MASK.GTS BTM_MASK.GBS TOP_SILK.GTO BTM_SILK.GBO BRD_EDGE.GBR BRD_DRILL.TXT
